0b5e3a48783481bc56d0fa5fb6db781136538727
郝博 <17600112422@163.com>
2019-12-28 23:12:30 +0800

    添加登录和home组件及路由导航

---
+++ src/components/home/home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>home组件</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/components/login/login'
+import Home from '@/components/Home/home'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      name: 'login',
+      path: '/login',
+      component: Login
+    },
+    {
+      name: 'home',
+      path: '/home',
+      component: Home
+    }
   ]
 })

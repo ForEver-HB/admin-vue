db895586bcf54a69cd0c5f9ea68770bddd2c5d63
郝博 <17600112422@163.com>
2019-12-31 22:40:32 +0800

    增加子路由路口及路由跳转

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,27 +1,78 @@
 <template>
-  <el-container class="container">
-  <el-header class="header">
+  <el-container class='container'>
+  <el-header class='header'>
     <el-row>
-      <el-col :span="4">
-        <div class="grid-content bg-purple">
-          <img src="http://img3.redocn.com/tupian/20180412/jinseguangmangxiantiaotuxingzimuzuhelogoheisebeijingzhanshi_9334315.jpg" alt="">
+      <el-col :span='4'>
+        <div class='grid-content bg-purple'>
+          <img src='http://img3.redocn.com/tupian/20180412/jinseguangmangxiantiaotuxingzimuzuhelogoheisebeijingzhanshi_9334315.jpg' alt=''>
         </div>
         </el-col>
-      <!-- <el-col :span="5">
-        <div class="grid-content bg-purple-light company_name">
+      <!-- <el-col :span='5'>
+        <div class='grid-content bg-purple-light company_name'>
           福润商城专卖
         </div>
       </el-col> -->
-      <el-col :offset="16" :span="4">
-        <div class="grid-content bg-purple closeToLogin">
-          <a href="#" @click.prevent="Logout">退出</a>
+      <el-col :offset='16' :span='4'>
+        <div class='grid-content bg-purple closeToLogin'>
+          <a href='#' @click.prevent='Logout'>退出</a>
         </div>
       </el-col>
     </el-row>
   </el-header>
-  <el-container class="container">
-    <el-aside class="aside" width="200px">Aside</el-aside>
-    <el-main class="main">Main</el-main>
+  <el-container class='container'>
+    <el-aside class='aside' width='200px'>
+      <el-menu
+      default-active='2'
+      class='el-menu-vertical-demo aside-menu'
+      @open='handleOpen'
+      @close='handleClose'
+      background-color='#545c64'
+      text-color='#fff'
+      :unique-opened='true'
+      active-text-color='#ffd04b'>
+      <el-submenu index='1'>
+        <template slot='title'>
+          <i class='el-icon-location'></i>
+          <span>导航管理</span>
+        </template>
+        <el-menu-item index='1-1'>用户列表</el-menu-item>
+      </el-submenu>
+      <el-submenu index='2'>
+        <template slot='title'>
+          <i class='el-icon-location'></i>
+          <span>权限管理</span>
+        </template>
+        <el-menu-item index='2-1'>角色列表</el-menu-item>
+        <el-menu-item index='2-2'>权限列表</el-menu-item>
+      </el-submenu>
+      <el-submenu index='3'>
+        <template slot='title'>
+          <i class='el-icon-location'></i>
+          <span>商品管理</span>
+        </template>
+        <el-menu-item index='3-1'>商品列表</el-menu-item>
+        <el-menu-item index='3-2'>商品分类</el-menu-item>
+        <el-menu-item index='3-3'>商品参数</el-menu-item>
+      </el-submenu>
+      <el-submenu index='4'>
+        <template slot='title'>
+          <i class='el-icon-location'></i>
+          <span>订单管理</span>
+        </template>
+        <el-menu-item index='4-1'>订单列表</el-menu-item>
+      </el-submenu>
+      <el-submenu index='5'>
+        <template slot='title'>
+          <i class='el-icon-location'></i>
+          <span>数据报表</span>
+        </template>
+        <el-menu-item index='5-1'>订单列表</el-menu-item>
+      </el-submenu>
+    </el-menu>
+    </el-aside>
+    <el-main class='main'>
+      <router-view></router-view>
+    </el-main>
   </el-container>
 </el-container>
 </template>
@@ -47,24 +98,29 @@ export default {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
-      })
-        .then(() => {
-          window.localStorage.removeItem('admin-token')
-          this.$router.push({
-            name: 'login'
-          })
-          this.$message({
-            type: 'success',
-            message: '退出登录成功!'
-          })
+      }).then(() => {
+        window.localStorage.removeItem('admin-token')
+        this.$router.push({
+          name: 'login'
         })
+        this.$message({
+          type: 'success',
+          message: '退出登录成功!'
+        })
+      })
+    },
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
     }
   }
 }
 </script>
 
 <style>
-.container {
+.container,.aside,.aside-menu {
   height: 100%;
 }
 .header {
@@ -80,7 +136,7 @@ export default {
   font-size: 22px;
   font-weight: bold;
   color: #fff;
-  font-family: "楷书";
+  font-family: '楷书';
 }
 .closeToLogin {
   text-align: center;
---
+++ src/components/role-list/role-list.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>角色列表</p>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>

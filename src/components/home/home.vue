5b7acadaea1f55569a1be6853a4cfaab7a69ccff
郝博 <17600112422@163.com>
2019-12-31 23:00:35 +0800

    左侧菜单支持点击调转

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -26,6 +26,7 @@
       class='el-menu-vertical-demo aside-menu'
       @open='handleOpen'
       @close='handleClose'
+      router="true"
       background-color='#545c64'
       text-color='#fff'
       :unique-opened='true'
@@ -33,16 +34,16 @@
       <el-submenu index='1'>
         <template slot='title'>
           <i class='el-icon-location'></i>
-          <span>导航管理</span>
+          <span>用户管理</span>
         </template>
-        <el-menu-item index='1-1'>用户列表</el-menu-item>
+        <el-menu-item index='/users'>用户列表</el-menu-item>
       </el-submenu>
       <el-submenu index='2'>
         <template slot='title'>
           <i class='el-icon-location'></i>
           <span>权限管理</span>
         </template>
-        <el-menu-item index='2-1'>角色列表</el-menu-item>
+        <el-menu-item index='/role'>角色列表</el-menu-item>
         <el-menu-item index='2-2'>权限列表</el-menu-item>
       </el-submenu>
       <el-submenu index='3'>
@@ -66,7 +67,7 @@
           <i class='el-icon-location'></i>
           <span>数据报表</span>
         </template>
-        <el-menu-item index='5-1'>订单列表</el-menu-item>
+        <el-menu-item index='5-1'>数据报表列表</el-menu-item>
       </el-submenu>
     </el-menu>
     </el-aside>
